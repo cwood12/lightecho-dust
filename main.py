a0346f83e5b39dbaf3cd7e503663ec544ba7c3b9
Charlotte Wood <cwood12@iastate.edu>
2023-06-01 13:59:41 -0500

    Updated scattering integral portion of code
    Replacing dust.py with main.py, which includes improvements to code for speed

--- main.py
+++ main.py
@@ -2,9 +2,9 @@
 Implement routine from Sugerman (2003); ApJ 126,1939
 "Observability of Scattered Light Echoes Around Variable Stars and Cataclysmic Events"
 
-Includes dust distribution routines from Weingartner & Draine (2001); ApJ 548,296 and Draine et al. (2021); ApJ 917,3. 
+Includes dust distribution routines from Weingartner & Draine (2001)[ApJ 548,296], Draine et al. (2021)[ApJ 917,3], and Hensley & Draine (2023)[ApJ 948,55]. 
 
-Optical properties of dust (Qsc, g) come from Draine & Hensley (2021); ApJ 909,94 (http://arks.princeton.edu/ark:/88435/dsp01qb98mj541) and Weingartner & Draine (2001); ApJ 548,296 (https://www.astro.princeton.edu/~draine/dust/dust.diel.html). 
+Optical properties of dust (Qsc, g, Csc) come from Draine & Hensley (2021)[ApJ 909,94] (http://arks.princeton.edu/ark:/88435/dsp01qb98mj541), Weingartner & Draine (2001)[ApJ 548,296] (https://www.astro.princeton.edu/~draine/dust/dust.diel.html), and Draine & Hensley (2023)[ApJ 948,55] (https://dataverse.harvard.edu/dataverse/astrodust). 
 """
 
 # __all__ = []
@@ -15,6 +15,7 @@ import numpy as np
 np.set_printoptions(precision=4, floatmode='fixed')
 import matplotlib.pyplot as plt
 plt.style.use('pg.mplstyle')
+import scipy.interpolate as interp
 import scipy.integrate as integrate
 from scipy.special import erf
 import astropy.units as u
@@ -23,20 +24,22 @@ import grainsizedist as gsd
 import dustconst as c
 import interpdust as id
 import scatintegral as scaint
+import hstfilters as hst
+
 
 #----------
+# Open data files
+#----------
 
-# All files downloaded from the above websites have been placed into a folder named "dustmodels" and file names are kept as-is
+# All files downloaded from the above websites have been placed into folders named 'dustmodels_WD01', 'dustmodels_DH21', and 'dustmodels_HD23'.
 # Constants set in dustconst.py assume preferred Milky Way dust model (Case A, Rv = 3.1, bc = 6e-5) from WD01
+
 
 # pull out available wavelengths for Qsc values, convert to cm from um, and take the log
 logwaveq = np.log10(1e-4*np.loadtxt('dustmodels/DH21_wave.dat')) #log(cm)
 # pull out available sizes for Qsc values, convert to cm from um, and take the log
 logsizeq = np.log10(1e-4*np.loadtxt('dustmodels/DH21_aeff.dat')) #log(cm)
 
-# set min, max wavelength and size for interpolation grid (in log space)
-minwave, maxwave = min(logwaveq), max(logwaveq)
-minsize, maxsize = min(logsizeq), max(logsizeq)
 
 # "For astrodust we assume a porosity P = 0.20" - Draine et al. (2021)
 # Astrodust with Fe = 0.10 only provided for b/a = 0.5 and 2.0
@@ -51,6 +54,7 @@ file1 = 'dustmodels/q_DH21Ad_P0.20_Fe0.00_0.500.dat'
 model1 = np.loadtxt(file1)
 model1_jori1, model1_jori2, model1_jori3 = id.jori(model1)
 
+
 # older models used for g (degree of forward scattering) values
 # carbonaceous dust
 carbong = 'dustmodels/Gra_81.dat'
@@ -64,18 +68,26 @@ logwaveg = np.log10(1e-4*np.loadtxt('dustmodels/LD93_wave.dat', unpack=True)) #l
 # pull out available sizes for the g values, convert to cm from um, and take the log
 logsizeg = np.log10(1e-4*np.loadtxt('dustmodels/LD93_aeff.dat', unpack=True)) #log(cm)
 
+
+#----------
+# Perform interpolation
 #----------
 
 # rearrange data into grids for interpolation
-# (wavelength, size) points and Qsc values for Model 1 and each orientation
-pointsq11, valuesq11 = id.point_val(model1_jori1, logwaveq, logsizeq)
-pointsq12, valuesq12 = id.point_val(model1_jori2, logwaveq, logsizeq)
-pointsq13, valuesq13 = id.point_val(model1_jori3, logwaveq, logsizeq)
+# (wavelength, size) points and Qsc values for Model 1 and Orientation 1
+pointsq11, valuesq11 = id.point_val_Qsc(logwaveq, logsizeq, model1_jori1)
+
 
 # (wavelength, size) points and g values for carbonaceous/PAH dust
 pointsgc, valuesgc = id.point_val_g(logwaveg, logsizeg, gcarb)
 # (wavelength, size) points and g values silicate/astro dust
 pointsgs, valuesgs = id.point_val_g(logwaveg, logsizeg, gsil)
+
+
+# set min, max wavelength and size for interpolation grid (in log space)
+minwave, maxwave = min(logwaveq), max(logwaveq)
+minsize, maxsize = min(logsizeq), max(logsizeq)
+print(10**minsize, 10**maxsize)
 
 # define size of grid in log space
 # step for wave already 0.005 in log space
@@ -90,17 +102,29 @@ y = np.arange(minsize, maxsize+stepy, stepy)
 pointsinterp = id.points_int(x, y)
 pointsinterp_nolog = id.points_int(10**x, 10**y)
 
+
 # use N-dimensional linear interpolator with log grid
 print('Interpolating Qscat...')
-model11_interp = id.lin2dinterp(pointsq11, valuesq11, pointsinterp)
-model12_interp = id.lin2dinterp(pointsq12, valuesq12, pointsinterp)
-model13_interp = id.lin2dinterp(pointsq13, valuesq13, pointsinterp)
+lininterp_Qsc = interp.LinearNDInterpolator(pointsq11, valuesq11, fill_value=0.00)
+# valuesint_Qsc = [lininterp_Qsc(point) for point in pointsinterp]
+valuesint_Qsc = lininterp_Qsc(pointsinterp)
+
 print('Interpolating g...')
-gcarb_interp = id.lin2dinterp(pointsgc, valuesgc, pointsinterp)
-gsil_interp = id.lin2dinterp(pointsgs, valuesgs, pointsinterp)
+# gcarb_interp = id.lin2dinterp(pointsgc, valuesgc, pointsinterp)
+# gsil_interp = id.lin2dinterp(pointsgs, valuesgs, pointsinterp)
+lininterp_gcarb = interp.LinearNDInterpolator(pointsgc, valuesgc, fill_value=0.00)
+# valuesint_gcarb = [lininterp_gcarb(point) for point in pointsinterp]
+valuesint_gcarb = lininterp_gcarb(pointsinterp)
+
+lininterp_gsil = interp.LinearNDInterpolator(pointsgs, valuesgs, fill_value=0.00)
+# valuesint_gsil = [lininterp_gsil(point) for point in pointsinterp]
+valuesint_gsil = lininterp_gsil(pointsinterp)
+
 
 #----------
-# calculate the grain size distribution for each dust type
+# Determine grain size distributions for each dust type
+#----------
+
 # carbonaceous/graphite and silicate from WD01
 # astrodust and PAHs from Draine et al. (2021)
 
@@ -136,15 +160,18 @@ plt.ylim(0.1, 100)
 plt.legend(ncol=1, loc='best')
 plt.show()
 
+
+#----------
+# Calculate integrated scattering function
 #----------
 
-# calculate integrated scattering function across all sizes for given wavelength and scattering angle (Eq. 8 from Sugerman 2003)
+# Eq. (8) from Sugerman (2003)
 
-# calculate rad (r from Fig. 1, Sugerman 2003) for echo in given observation and convert to cm
+# calculate radius (r from Fig. 1, Sugerman 2003) for echo in given observation
 rad = np.sqrt(c.pecho**2 + c.z**2) #ly
 radcm = rad.to(u.cm)
 
-# set wavelength (remove when integrating over all wavelengths)
+# set wavelength (if only evaluating at one wavelength)
 wave = 10**x[300]*u.cm #cm
 print('Wavelength = ' + str(wave))
 
@@ -153,71 +180,11 @@ scatangle = scaint.scatangle(radcm)
 mu = np.cos(scatangle)
 print('Mu = ' + str(mu))
 
-# remove log from x (wave) & y (size) arrays, add units
+# remove log from x/wavelength and y/size arrays and add units
 x_nolog = 10**x * u.cm
 y_nolog = 10**y * u.cm
 
+# integrate across
 print('Performing scattering integration...')
-# Model 1, orientation 1
-# ds_array = []
-# int_sizes = []
-ds_array = np.zeros_like(y_nolog)
-for i,size in enumerate(y_nolog):
-# for size in y_nolog:
-    if size.value >= 3.5e-8 and size.value <= 1.0e-4:
-        g = scaint.extract_g(wave.value, size.value, pointsinterp_nolog, gcarb_interp)[0]
-        Qscat = scaint.extract_Qsc(wave.value, size.value, pointsinterp_nolog, model11_interp)[0]
 
-        phi = scaint.Phi(mu, g)
-
-        f = gsd.Dist_carb(size, B1, B2)
-        dS = scaint.dS(size, Qscat, phi, f)
-        # int_sizes.append(size.value)
-        # ds_array.append(dS.value)
-        ds_array[i] = dS
-
-int_result = integrate.trapezoid(ds_array, y_nolog)*u.cm**2
-print(int_result)
-
-#----------
-
-
-
-
-
-
-# # Model 1, orientation 2
-# ds_array = []
-# int_sizes = []
-# for size in y_nolog:
-#     if size.value >= 3.5e-8 and size.value <= 1.0e-4:
-#         g = scaint.extract_g(wave.value, size.value, pointsinterp_nolog, gcarb_interp)[0]
-#         Qscat = scaint.extract_Qsc(wave.value, size.value, pointsinterp_nolog, model12_interp)[0]
-
-#         phi = scaint.Phi(mu, g)
-
-#         f = gsd.Dist_carb(size, B1, B2)
-#         dS = scaint.dS(size, Qscat, phi, f)
-#         int_sizes.append(size.value)
-#         ds_array.append(dS.value)
-
-# int_result = integrate.trapezoid(ds_array, int_sizes)*u.cm**2
-# print(int_result)
-
-# # Model 1, orientation 3
-# ds_array = []
-# int_sizes = []
-# for size in y_nolog:
-#     if size.value >= 3.5e-8 and size.value <= 1.0e-4:
-#         g = scaint.extract_g(wave.value, size.value, pointsinterp_nolog, gcarb_interp)[0]
-#         Qscat = scaint.extract_Qsc(wave.value, size.value, pointsinterp_nolog, model13_interp)[0]
-
-#         phi = scaint.Phi(mu, g)
-
-#         f = gsd.Dist_carb(size, B1, B2)
-#         dS = scaint.dS(size, Qscat, phi, f)
-#         int_sizes.append(size.value)
-#         ds_array.append(dS.value)
-
-# int_result = integrate.trapezoid(ds_array, int_sizes)*u.cm**2
-# print(int_result)
+# note with new data from Hensley & Draine (2023), values given as Csca, which is Qsca * pi * a^2. In integrated scattering function, replace Qsc*sigma with Csca.
